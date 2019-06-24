5fe3ab29c39e1119d297bba351f2ede50c6158ba
Geetaku <44674988+superpcwang@users.noreply.github.com>
2019-06-25 00:58:22 +0800

    Update about.

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -22,16 +22,16 @@ class ContactData {
 export class ContactComponent implements OnInit {
     
     public primaryDatas: ContactData[] = [
-        new ContactData("Gmail", "assets/img/GmailLogoFull.png", "Email info Email info Email info", "http://www.google.com"),
-        new ContactData("Discord", "assets/img/DiscordLogoFull.png", "Discord info", "http://www.google.com"),
-        new ContactData("Github", "assets/img/GithubLogoFull.png", "Github info", "http://www.google.com"),
-        new ContactData("Plurk", "assets/img/PlurkLogoFull.png", "Plurk info", "http://www.google.com"),
-        new ContactData("YouTube", "assets/img/YoutubeLogoFull.png", "YouTube info", "http://www.google.com"),
-        new ContactData("Twitch", "assets/img/TwitchLogoFull.png", "Twitch info", "http://www.google.com"),
-        new ContactData("Pixiv", "assets/img/PixivLogoFull.png", "Pixiv info", "http://www.google.com"),
-        new ContactData("Linkedin", "assets/img/LinkedinLogoFull.png", "Linkedin info", "http://www.google.com"),
-        new ContactData("Flickr", "assets/img/FlickrLogoFull.png", "Flickr info", "http://www.google.com"),
-        new ContactData("Facebook", "assets/img/FacebookLogoFull.png", "Facebook info", "http://www.google.com")
+        new ContactData("Gmail", "assets/img/GmailLogoFull.png", "常用信箱每日收", "mailto:superpcwang@gmail.com"),
+        new ContactData("Discord", "assets/img/DiscordLogoFull.png", "線上及時應答", "https://discord.gg/sSzb56G"),
+        new ContactData("YouTube", "assets/img/YoutubeLogoFull.png", "暫不開放", "http://www.google.com"),
+        new ContactData("Twitch", "assets/img/TwitchLogoFull.png", "暫不開放", "http://www.google.com"),
+        new ContactData("Github", "assets/img/GithubLogoFull.png", "程式回收場(?)", "https://github.com/superpcwang"),
+        new ContactData("Pixiv", "assets/img/PixivLogoFull.png", "暫不開放", "http://www.google.com"),
+        new ContactData("Flickr", "assets/img/FlickrLogoFull.png", "暫不開放", "http://www.google.com"),
+        new ContactData("Linkedin", "assets/img/LinkedinLogoFull.png", "暫不開放", "http://www.google.com"),
+        new ContactData("Plurk", "assets/img/PlurkLogoFull.png", "暫不開放", "http://www.google.com"),
+        new ContactData("Facebook", "assets/img/FacebookLogoFull.png", "暫不開放", "http://www.google.com"),
         
     ];
 
